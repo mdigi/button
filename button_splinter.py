f06c755df4cae51051d760fb29b8c4cf37862ff8
unknown <mdegaetano@MDEGAETANO-LTW7.vicorpower.vicr.com>
2015-04-03 11:53:03 -0400

    button_splinter will now refresh page if button can't be found

--- button_splinter.py
+++ button_splinter.py
@@ -4,19 +4,37 @@
 from splinter import Browser
 import time
 
-with Browser() as page:
-    url = 'http://algersoft.net/kriegerathome/'
-    page.visit(url)
-
+def attack(page):
+    """
+    Helper function to try and push the attack button. If the page has not
+    refreshed, then wait and keep trying.
+    """
     while 1:
-        # wait 30 sec, refresh page, wait 2 more seconds
-        time.sleep(30)
-        page.reload()
-        time.sleep(2)
+        try:
+            button = page.find_by_id('worker_action')
+            button.click()
+            break
+        except:
+            page.reload()
+            time.sleep(3)
+            print 'waiting for page to refresh'
 
-        # click the attack button
-        button = page.find_by_id('worker_action')
-        print 'click!'
-        button.click()
+def push_button():
+    """
+    Navigate to kriegerathome and push the attack button every 30 seconds
+    """
+    with Browser() as page:
+        url = 'http://algersoft.net/kriegerathome/'
+        page.visit(url)
 
+        while 1:
+            # wait 30 sec, refresh page, wait 2 more seconds
+            time.sleep(30)
+            page.reload()
+            time.sleep(2)
 
+            # click the attack button
+            attack(page)
+            print 'click!'
+
+push_button()
